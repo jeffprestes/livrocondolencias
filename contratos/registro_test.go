c1446834e3ed8f92f839d5e53160a8e45a040d10
Jeff Prestes <jeffprestes@gmail.com>
2018-07-03 07:50:22 -0300

    Add funcoes de teste para obter o registro e escrever um novo registro

--- contratos/registro_test.go
+++ contratos/registro_test.go
@@ -29,3 +29,37 @@ func TestDeployRegistro(t *testing.T) {
 		t.Error("Um endereco valido era esperado. Recebido um endereco vazio")
 	}
 }
+
+func TestGetRegistro(t *testing.T) {
+	key, _ := crypto.GenerateKey()
+	auth := bind.NewKeyedTransactor(key)
+	alloc := make(core.GenesisAlloc)
+	alloc[auth.From] = core.GenesisAccount{Balance: big.NewInt(100000000000)}
+	blockchain := backends.NewSimulatedBackend(alloc)
+	_, _, contrato, _ := DeployRegistro(auth, blockchain)
+	blockchain.Commit()
+
+	if msg, _ := contrato.Mensagem(nil); msg != "Uma boa e pacifica morte para todos..." {
+		t.Errorf("Esperava-se a mensagem: Uma boa e pacifica morte para todos... e o retorno foi: %s\n", msg)
+	}
+}
+
+func TestSetRegistro(t *testing.T) {
+	key, _ := crypto.GenerateKey()
+	auth := bind.NewKeyedTransactor(key)
+	alloc := make(core.GenesisAlloc)
+	alloc[auth.From] = core.GenesisAccount{Balance: big.NewInt(100000000000)}
+	blockchain := backends.NewSimulatedBackend(alloc)
+	_, _, contrato, _ := DeployRegistro(auth, blockchain)
+	blockchain.Commit()
+	opcoesTransacao := &bind.TransactOpts{
+		From:   auth.From,
+		Signer: auth.Signer,
+		Value:  nil,
+	}
+	contrato.RegistrarMensagem(opcoesTransacao, "Uma passagem pacifica...")
+	blockchain.Commit()
+	if msg, _ := contrato.Mensagem(nil); msg != "Uma passagem pacifica..." {
+		t.Errorf("Esperava-se a mensagem: Uma passagem pacifica... e o retorno foi: %s\n", msg)
+	}
+}
